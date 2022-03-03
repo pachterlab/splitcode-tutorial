5c5d12543b5e4f2019d3a7c164c2072e4fa07d2f
Delaney Sullivan <delaneyk.sullivan@gmail.com>
2022-03-03 03:57:07 -0800

    Updated conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'splitcode'
+copyright = '2022, Delaney Sullivan'
+author = 'Delaney Sullivan'
 
 release = '0.1'
-version = '0.1.0'
+master_doc = 'index'
 
 # -- General configuration
 
