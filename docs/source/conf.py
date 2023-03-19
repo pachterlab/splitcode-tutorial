6b26837341550652c945e14bf8c45bf5f483b363
Delaney Sullivan <delaneyk.sullivan@gmail.com>
2023-03-19 01:58:57 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,9 @@ extensions = [
     'sphinx.ext.autosectionlabel',
 ]
 
+autosectionlabel_prefix_document = True
+autosectionlabel_maxdepth = 2
+
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
